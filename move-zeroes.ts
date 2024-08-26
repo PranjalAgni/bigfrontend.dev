04e5de1426ec1cef5e262b7bbb744c019263b549
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-08-26 23:47:45 +0530

    :airplane: okayish coding problem

---
+++ move-zeroes.ts
@@ -0,0 +1,20 @@
+// https://bigfrontend.dev/problem/move-zeros
+
+// Time: O(N) | Space: O(1)
+function moveZeros(list: Array<any>): void {
+  // your code here
+  let pos = 0;
+  let idx = 0;
+  const N = list.length;
+  while (idx < N) {
+    const elt = list[idx];
+    list[pos] = elt;
+    if (elt !== 0) pos += 1;
+    idx += 1;
+  }
+
+  while (pos < N) {
+    list[pos] = 0;
+    pos += 1;
+  }
+}
