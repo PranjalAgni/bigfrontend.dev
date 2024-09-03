c7190b9eb7a899b66d1b0a2e1249313b1c755a69
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-09-03 23:58:13 +0530

    :hammer: still wip trying to solve it

---
+++ improve-a-function.js
@@ -0,0 +1,35 @@
+// https://bigfrontend.dev/problem/improve-a-function
+// Currently wip
+// TC: O(N) | SC: O(N)
+/**
+ * @param {object[]} items
+ * @excludes { Array< {k: string, v: any} >} excludes
+ */
+
+/**
+ * @param {object[]} items
+ * @param { Array< {k: string, v: any} >} excludes
+ * @return {object[]}
+ */
+function excludeItems(items, excludes) {
+  const answer = [];
+  excludes.forEach((pair) => {
+    const current = items.filter((item) => !(item[pair.k] === pair.v));
+    answer.push(...current);
+  });
+
+  return answer;
+}
+// function excludeItems(items, excludes) {
+//   const answer = [];
+//   const N = items.length;
+//   const excludeMap = new Map();
+//   for (const exclude of excludes) {
+//     excludeMap.set(`${exclude.k}_${exclude.v}`, true);
+//   }
+
+//   for (let idx = 0; idx < N; idx++) {
+//     const item = items[idx];
+//     const
+//   }
+// }
