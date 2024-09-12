6e99bbb2aab17b0113b0f2d9cc92ae70b5ae4290
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-09-12 23:05:08 +0530

    :pinched_fingers: good old binary search question

---
+++ search-last-index-with-Binary-Search-possible-duplicate-array.js
@@ -0,0 +1,28 @@
+// https://bigfrontend.dev/problem/search-last-index-with-Binary-Search-possible-duplicate-array
+// TC: O(logN) | Space: O(1)
+
+/**
+ * @param {number[]} arr - ascending array with duplicates
+ * @param {number} target
+ * @return {number}
+ */
+function lastIndex(arr, target) {
+  const N = arr.length;
+  let low = 0;
+  let high = N;
+  let answer = -1;
+
+  while (low < high) {
+    const mid = low + Math.floor((high - low) / 2);
+    if (arr[mid] === target) {
+      answer = Math.max(mid, answer);
+      low = mid + 1;
+    } else if (arr[mid] > target) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return answer;
+}
