7c84cc5a38e67cb030516f996b36fe978fbaf794
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-09-20 00:18:46 +0530

    :tada: solved this beautiful question nice concept

--- implement-sum.cpp
+++ implement-sum.cpp
@@ -1,10 +1,14 @@
+// https://bigfrontend.dev/problem/create-a-sum
+// TC: O(N) | Space: O(1)
 /**
  * @param {number} num
  */
 function sum(num) {
-  let total = num;
-  return (val) => {
-    total += val;
-    return total;
+  const f = (num2) => {
+    if (num2) return sum(num + num2);
+    else return num;
   }
+
+  f.valueOf = () => num;
+  return f;
 }
