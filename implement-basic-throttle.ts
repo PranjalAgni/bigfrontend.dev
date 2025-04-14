aad012aa98c3d5748ae8d07d7f54d6fc9d6f595c
PranjalAgni <pranjal.agnihotri6@gmail.com>
2025-04-15 00:27:35 +0530

    :icecream: nice and fancy throttling solution

--- implement-basic-throttle.ts
+++ implement-basic-throttle.ts
@@ -1,6 +1,24 @@
 // https://bigfrontend.dev/problem/implement-basic-throttle
 
-function throttle<T extends (...args: any[]) => any>(
-  func: T,
-  wait: number
-): T {}
+function throttle<T extends (...args: any[]) => any>(func: T, wait: number): T {
+  let cooling = false;
+  let result: any = null;
+  let lastArgs: any = null;
+  return function (...args: any[]) {
+    if (!cooling) {
+      cooling = true;
+      result = func(...args);
+      setTimeout(() => {
+        cooling = false;
+        if (lastArgs) {
+          func(...lastArgs);
+          lastArgs = null;
+        }
+      }, wait);
+    } else {
+      lastArgs = args;
+    }
+
+    return result;
+  } as T;
+}
