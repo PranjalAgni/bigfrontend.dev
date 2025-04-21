6d780446fde632822e59c281d60c14e5fa8fad3e
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-04-21 23:58:57 +0530

    :thinking: trying to figure it out

--- implement-throttle-with-leading-and-trailing-option.ts
+++ implement-throttle-with-leading-and-trailing-option.ts
@@ -1,6 +1,8 @@
 // https://bigfrontend.dev/problem/implement-throttle-with-leading-and-trailing-option
 // This is a Typescript coding problem from BFE.dev
 
+// This is a JavaScript coding problem from BFE.dev
+
 function throttle<T extends (...args: any[]) => any>(
   func: T,
   wait: number,
@@ -10,4 +12,35 @@ function throttle<T extends (...args: any[]) => any>(
   }
 ): T {
   // your code here
+  let cooling = false;
+  let lastArgs = null;
+  let result = null;
+
+  return function (...args: any[]): T {
+    if (!cooling) {
+      cooling = true;
+
+      if (option.leading) {
+        result = func(...args);
+      }
+
+      setTimeout(() => {
+        cooling = false;
+        if (option.trailing) {
+          result = func(...args);
+        }
+
+        if (lastArgs) {
+          if (option.leading || option.trailing) {
+            result = func(...lastArgs);
+          }
+          lastArgs = null;
+        }
+      }, wait);
+    } else {
+      lastArgs = args;
+    }
+
+    return result;
+  };
 }
