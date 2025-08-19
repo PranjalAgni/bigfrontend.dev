ebcf269c193b0a55e74092b224282bdbfebbf069
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-08-19 09:39:45 +0530

    :sparkles: solved a stack related problem

--- remove-characters.js
+++ remove-characters.js
@@ -1,13 +1,29 @@
+// https://bigfrontend.dev/problem/remove-characters
+
+// Time: O(n) | Space: O(N)
 function removeChars(input: string): string {
   const stack = [];
   for (const char of input) {
-    if (char !== 'c') {
+    if (char !== 'b') {
       stack.push(char);
     }
   }
 
   // some manipulation logic here
-  while (stack.length > 0) {
-    // ??
+  let isFound = stack.length > 0;
+  while (isFound) {
+    isFound = false;
+    for (let idx = 1; idx < stack.length; idx++) {
+      if (stack[idx] === 'c' && stack[idx - 1] === 'a') {
+        isFound = true;
+        stack.splice(idx - 1, 2);
+      }
+    }
   }
+
+  if (!stack.length) {
+    stack.push('');
+  }
+
+  return stack.join('');
 }
