eb19d534811ed9c9f473d17f029502b26a810057
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-10-23 01:12:27 +0530

    :cloud: good set theory problem

--- array-intersect.js
+++ array-intersect.js
@@ -1,3 +1,18 @@
 function getIntersection(arr1: any[], arr2: any[]): any[] {
   // your code here
+  const answer = [];
+  const map = new Map();
+  for (const elt of arr1) {
+    if (!map.has(elt)) map.set(elt, 0);
+    map.set(elt, map.get(elt) + 1);
+  }
+
+  for (const elt of arr2) {
+    if (map.has(elt) && map.get(elt) > 0) {
+      answer.push(elt);
+      map.set(elt, 0);
+    }
+  }
+
+  return answer;
 }
